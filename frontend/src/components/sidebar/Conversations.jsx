99fcf16ce58b6e7f08175a8fc84f01cae08a034e
shahbaz <shahbazsiddiqui250@email.com>
2024-10-08 21:52:34 +0530

    how people came up with such stuff💀

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -4,7 +4,7 @@ import useGetConversations from '../../hooks/useGetConversations';
 import { getRandomEmoji } from '../../utils/emojis';
 const Conversations = () => {
   const{loading,conversations}=useGetConversations();
-  console.log("CONVERSATIONS:",conversations);
+  
 
   return (
     <div className='py-2 flex flex-col overflow-auto '>
