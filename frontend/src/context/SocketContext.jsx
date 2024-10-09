9871ab03a8bf5fc3db1fccc6675852965066017d
shahbaz <shahbazsiddiqui250@email.com>
2024-10-09 23:54:17 +0530

    deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://simple-chat-app-sgqf.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
