f7cb97e3d3a5db7051968a714149eebd242fa2c9
shahbaz <shahbazsiddiqui250@email.com>
2024-10-10 16:22:59 +0530

    typo fix 1

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://simple-chat-app-sgqf.onrender.com/", {
+			const socket = io("https://simple-chat-app-nqph.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
